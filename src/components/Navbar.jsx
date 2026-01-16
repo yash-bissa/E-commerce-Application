871359e1ed76ab9b6aa65afb30ac6fbb1734ae93
yash-bissa <yashkumar100404@gmail.com>
2026-01-16 18:58:27 +0530

    Made the Hero section and also added the latest collection section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <div className='flex justify-between items-center py-5 font-medium'>
-        <img src={assets.logo} className='w-36' alt="" />
+        <Link to='/'><img src={assets.logo} className='w-36' alt="" /></Link>
 
         {/* ------- Center Navigation ------- */}
         <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
