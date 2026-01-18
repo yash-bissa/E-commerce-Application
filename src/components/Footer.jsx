862665dc0070e5c5040f3e34608ca4096d9e5d89
yash-bissa <yashkumar100404@gmail.com>
2026-01-18 22:52:59 +0530

    COmpleted the home page and added footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import {assets} from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 mt-40 text-sm'>
+            <div>
+                <img src={assets.logo} className='mb-5 w-32' alt="" />
+                <p className='w-full md:w-2/3 text-gray-600'>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti, placeat, mollitia iusto beatae porro ab veritatis, iste tempore corporis maxime necessitatibus voluptates illum delectus nihil! Itaque molestias iure quibusdam. Laudantium!
+                </p>
+            </div>
+
+            <div>
+                <p className='text-xl font-medium mb-5'>COMPANY</p>
+                <ul className='flex flex-col gap-1 text-gray-600'>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+
+            <div>
+                <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+                <ul className='flex flex-col gap-1 text-gray-600'>
+                    <li>+1-212-456-897</li>
+                    <li> contact us at : forever@gmail.com</li>
+                </ul>
+            </div>
+        </div>
+
+            <div>
+                <hr />
+                <p className='py-5 text-sm text-center'>Copyright 2026@ forever.com - All Rights Reserved</p>
+            </div>
+
+    </div>
+  )
+}
+
+export default Footer
